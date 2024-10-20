4c40b90bd8d658ff11683868f5de36fbcab6f9d5
Haroutyun Harry Altunyan <haroutyunhaltunyan93@icloud.com>
2024-10-20 04:31:26 -0700

    css upgrade, hrefs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-// Mobile menu toggle
-console.log("Hello World"); // Add this to make sure your JS is connected
+// Console log to verify JavaScript connection
+console.log("Hello World");
 
+// Toggle mobile menu (burger icon)
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#navbarBasicExample');
-
 burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active');  // Toggle the 'is-active' class to open/close the menu
+    navbarMenu.classList.toggle('is-active');
+});
+
+// Toggle 'More' dropdown inside the mobile menu
+const moreDropdownContainer = document.querySelector('#moreDropdownContainer');
+const moreLink = document.querySelector('#moreDropdown');
+
+// Add event listener to "More" to toggle dropdown
+moreLink.addEventListener('click', (e) => {
+    e.preventDefault(); // Prevent default behavior
+    moreDropdownContainer.classList.toggle('is-active'); // Toggle dropdown visibility
 });
